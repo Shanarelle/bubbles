98050cfe1d7c26de1458fca85173ec88afc63e8a
Shanarelle <shanarelle@liquidlability.ninja>
2017-11-05 08:04:30 +0000

    can move the blob - it stretches horizontally when you're moving. no diagonal support

---
+++ README.md
@@ -0,0 +1,25 @@
+README
+
+This is to keep track of the state of the penguin herding game
+
+
+
+Current State
+-------------
+- have some chicks and sitter on screen
+- chicks wander about the screen - properly bounded by canvas
+- chicks and sitter have portraits
+
+
+
+Next Steps
+----------
+- get chicks to react to proximity of the sitter
+- allow sitter to move diagonally
+- put in a pen
+- add check for penned to chick so once its inside the pen it stays within its confines
+
+- create and add music
+- add animation to sitter so it flaps it arms when near a chick?
+- create pen tiles so not just a line
+- create background image? 
---
+++ sketch.js
@@ -0,0 +1,125 @@
+
+var octo_image;
+var octo;
+var bubbles;
+var canvas_width = 900;
+var canvas_height = 580;
+var paused = false;
+
+// pre-fetch all required resources
+// function preload() { 
+//   player_image = loadImage('assets/');
+// }
+
+
+function setup() {
+	var myCanvas = createCanvas(canvas_width, canvas_height);
+	myCanvas.parent('canvasContainer');
+
+	player = new Octopus(canvas_width/2, canvas_height/2);
+}
+
+function draw() {
+	background('#07357f');
+
+	player.update();
+
+	// chicks.forEach(function(chick) { chick.update() });
+}
+
+function keyTyped() {
+  if (key === 'p') {
+  	paused = !paused;
+  	paused ? noLoop() : loop();
+  	console.log(paused);
+  }
+  return false;
+}
+
+
+function Octopus(x, y) {
+	this.position = createVector(x, y);
+	this.velocity = createVector(0, 0);
+}
+
+Octopus.prototype.update = function() {
+	var changeforce = readInput();
+
+	if (changeforce.mag() > 0) {
+		this.velocity = this.velocity.add(changeforce);
+		this.velocity = this.velocity.limit(10);
+	}
+
+	this.position = this.position.add(this.velocity);
+
+	this.velocity = this.velocity.mult(0.9);
+
+	this.draw();
+}
+
+Octopus.prototype.draw = function() {
+	// console.log(this.velocity);
+
+	fill('#3232ba');
+
+	ellipse(this.position.x, this.position.y, (this.velocity.mag() * 8) + 20, 50);
+	// image(this.me, this.position.x, this.position.y, 52, 67);
+}
+
+function readInput() {
+	//TODO: adjust to allow diagonal swimming
+	var right = 0;
+	var down = 0;
+	if (keyIsDown(RIGHT_ARROW)) {
+		right = 2;
+	} else if (keyIsDown(LEFT_ARROW)) {
+		right = -2;
+	} else if (keyIsDown(DOWN_ARROW)) {
+		down = 2;
+	} else if (keyIsDown(UP_ARROW)) {
+		down = -2;
+	}
+	return createVector(right, down);
+}
+
+// function Chick() {
+// 	this.me = chick_image;
+// 	this.velocity = createVector(random(-0.2,0.2),random(-0.2,0.2));
+// 	this.position = createVector(random(width-50),random(height-50));
+// 	this.box = {'x_min':0, 'x_max':canvas_width-20, 'y_min':0, 'y_max':canvas_height-28};
+// }
+
+// Chick.prototype.update = function() {
+// 	var steer = random(-QUARTER_PI/4, QUARTER_PI/4);
+// 	this.velocity.rotate(steer);
+
+// 	if (steer < abs(0.1)) {
+// 		if (this.velocity.mag() < 2.5) {
+// 			this.velocity.mult(1.1);
+// 		} else {
+// 			this.velocity.mult(0.9);
+// 		}
+// 	} else {
+// 		this.velocity.mult(0.8);
+// 	}
+
+
+// 	if(this.position.x < this.box.x_min || this.position.x > this.box.x_max) {
+// 		this.velocity.rotate(HALF_PI)
+// 	}
+// 	if(this.position.y < this.box.y_min || this.position.y > this.box.y_max) {
+// 		this.velocity.rotate(HALF_PI)
+// 	}
+
+// 	this.position.add(this.velocity);
+
+// 	this.draw();
+// }
+
+// Chick.prototype.draw = function() {
+// 	image(this.me, 
+// 		this.position.x, 
+// 		this.position.y, 
+// 		20, 
+// 		28);
+// }
